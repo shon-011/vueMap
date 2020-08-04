661f1b2f6cb6bb58ca2b33d657c29ba3e792c03d
shona <f.mb@outlook.jp>
2020-08-05 00:17:08 +0900

    vuetify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-
-
   </div>
 </template>
 <script>
@@ -10,11 +8,15 @@ import axios from "axios";
 
 export default {
   mounted() {
-    console.log('aaa');
-    axios.get("https://api.nytimes.com/svc/topstories/v2/world.json?api-key=jlglButOWZ4NJmK7A2sYT74hHoljdd0U")
-    .then(res => {
-      console.log(res);
-    })
+    console.log("aaa");
+    axios
+      .get(
+        "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=jlglButOWZ4NJmK7A2sYT74hHoljdd0U"
+      )
+      .then(res => {
+        console.log(res);
+      });
   }
-}
-</script>>
+};
+</script>
+>
